7ca2d4a52dbfd079cc01ea16e33d5cd3141d0306
anaki <raeji@naver.com>
2017-03-21 08:26:57 +0900

    Update withyourName.cpp

--- withyourName.cpp
+++ withyourName.cpp
@@ -3,6 +3,11 @@
 int main() {
   printf("Hello Jonghwa");
   
-  printf("Hello Mina");
-  printf("Hello Anaki");
+  printf("Hello Mina\n");
+  printf("Hello Anaki\n");
+  printf("Hello Vega\n");
+  Printf("Hello Mr.Bear\n");
+  printf("Hello Snoopy\n");
+  printf("Hello Munoki\n")
+  printf("Hello Squirrel\n");
 }
